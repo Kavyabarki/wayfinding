3edee192dfd9e875e2aed6e588e6f85ceddb3a02
Kavya Barki <kavyabarki15@mail.com>
2026-05-26 11:52:43 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Navbar.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home'; 
+
+function Navbar() {
+  return (
+    <nav
+      className="navbar"
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr auto',
+        gridTemplateAreas: '"logo center profile"',
+        alignItems: 'center',
+        padding: '10px 20px',
+        backgroundColor: 'white',
+        boxShadow: '0px 2px 10px rgba(0, 0, 0, 0.1)',
+      }}
+    >
+   
+      <div className="navbar-logo" style={{ gridArea: 'logo' }}>
+        <img
+          src="/way.png"
+          alt="Logo 3"
+          style={{ height: '50px', cursor: 'pointer' }}
+       
+        />
+      </div>
+
+
+
+     
+      <div
+        className="navbar-profile"
+        style={{
+          gridArea: 'profile',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-end',
+        }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <AccountCircleIcon style={{ color: '#17a2b8', marginRight: '10px', fontSize: '40px' }} />
+          <span className="navbar-welcome-message" style={{ fontSize: '16px', fontWeight: 'bold' }}>
+            Hello, Admin
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
